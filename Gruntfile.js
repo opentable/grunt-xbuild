edfdd689bf7f0a95ea24d0d8e7146f6a0b508e43
Andy Royle <ajroyle@gmail.com>
2013-08-12 22:47:27 +0100

    tidy up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,24 +6,15 @@ module.exports = function(grunt) {
     jshint: {
       all: [
         'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
+        'tasks/*.js'
       ],
       options: {
         jshintrc: '.jshintrc'
       }
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
     }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  grunt.registerTask('test', 'nodeunit');
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', ['jshint']);
 };
